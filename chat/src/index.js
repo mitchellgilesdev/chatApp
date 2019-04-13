c6f7510a1098709509ba853e22af13f6f0b8ced6
mitchellgilesdev <47744016+mitchellgilesdev@users.noreply.github.com>
2019-04-13 12:02:30 +0800

    a

--- chat/src/index.js
+++ chat/src/index.js
@@ -25,8 +25,28 @@ let nextMessageId = 0
 const nextUserId = 0
 
 export const addMessage = (message,author) => ({
+
 type: types.ADD_MESSAGE,
 id: nextMessageId++,
 message,
 author,
+})
+
+export const addUser = name => ({
+    type: types.ADD_USER,
+    id: nextUserId++,
+    name
+})
+
+export const messageRecieved = (message, author) => ({
+    type: types.MESSAGE_RECIEVED,
+    id: nextMessageId++,
+    message,
+    author
+})
+
+export const populateUsersList = users => ({
+
+    type: types.USERS_LIST,
+    users
 })
