d81b7214dd7d74b4c350ec4c3fe496de46b8b2cb
mitchellgilesdev <47744016+mitchellgilesdev@users.noreply.github.com>
2019-04-13 11:49:45 +0800

    fix react import and index.js

--- chat/src/index.js
+++ chat/src/index.js
@@ -5,11 +5,15 @@ import {createStore} from 'redux'
 
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+import registerServiceWorker from './registerServiceWorker'
+import chat from './reducers'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(chat)
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render(
+    <Provider store={store}>
+     <App />
+    </Provider>,
+    doccument.getElementById('root')
+)
+
